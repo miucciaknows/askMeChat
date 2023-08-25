9f6482998789426fabbd80333ff9f053575a2d27
Nathalia Trazzi <miumiutrassi@gmail.com>
2023-08-25 11:13:49 -0300

    Update Main.storyboard, ViewController.swift

--- askMeChat/askMeChat/ViewController.swift
+++ askMeChat/askMeChat/ViewController.swift
@@ -13,42 +13,47 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var respostaAssistant: UITextView!
     
+    @IBOutlet weak var inputUsuario: UITextView!
+    
+    
     @IBAction func buscarResposta(_ sender: UIButton) {
         //let apiKey = processInfo.environment["apiKey"]
         let apiKey = ""
+        inputUsuario.text = digiteSuaPergunta.text
         guard let question = digiteSuaPergunta.text else {
             return
     }
+  
 
         respostaAssistant.isEditable = false
         respostaAssistant.isScrollEnabled = false
-        
-        
+             
         let url = URL(string: "")!
         var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.setValue(apiKey, forHTTPHeaderField: "apikey")
-        
+             request.httpMethod = "POST"
+             request.setValue("application/json",
+                    forHTTPHeaderField: "Content-Type")
+             request.setValue(apiKey, forHTTPHeaderField: "apikey")
+             
         let parameters: [String: Any] = [
             "question": question,
             "user_session": [
             "metadata": [
             "user_id": "string"
-                          ],
+                     ],
             "system": [
                 "session_id": "string"
-                          ]
-                      ]
-                  ]
+                     ]
+                 ]
+             ]
         request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
-                  
+             
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error {
-                print("Error:", error.localizedDescription)
+        print("Error:", error.localizedDescription)
                 return
-                      }
-                      
+                 }
+                 
             if let data = data {
                 do {
                     let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
@@ -56,16 +61,15 @@ class ViewController: UIViewController {
                         print("Resposta:", answer)
                         DispatchQueue.main.sync {
                             self.respostaAssistant.text = answer
-                        }
-                              }
-                          } catch {
-                              print("Error parsing JSON:", error.localizedDescription)
-                          }
-                      }
-                  }.resume()
-              }
-    }
-
-
-        
-    
+                             }
+                         }
+                     } catch {
+                         print("Error parsing JSON:", error.localizedDescription)
+                     }
+                 }
+             }.resume()
+             
+             
+             digiteSuaPergunta.text = ""
+         }
+     }
