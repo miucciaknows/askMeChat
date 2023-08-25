3604a891e9c4f811fdd21b07d024ba8055f71645
Nathalia Trazzi <miumiutrassi@gmail.com>
2023-08-25 11:54:44 -0300

    add PreviousQuestionsController file

---
+++ askMeChat/askMeChat/PreviousQuestionsController.swift
@@ -0,0 +1,17 @@
+//
+//  PreviousQuestionsController.swift
+//  askMeChat
+//
+//  Created by Nathalia Trazzi on 25/08/23.
+//
+
+import Foundation
+import UIKit
+
+class PreviousQuestionsController: UIViewController {
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+}
