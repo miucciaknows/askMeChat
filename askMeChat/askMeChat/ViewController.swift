3c15625fc98bd2b2a77c983767a9ec906a19f9c6
Nathalia Trazzi <32164221@mackenzista.com.br>
2023-08-26 19:25:06 -0300

    Update ViewController, Main.storyboard
    Add functions to the text field expand along with the keyboard, code organization and some storyboard changes

--- askMeChat/askMeChat/ViewController.swift
+++ askMeChat/askMeChat/ViewController.swift
@@ -8,26 +8,46 @@
 import UIKit
 import Foundation
 
-class ViewController: UIViewController {
-    
-    override func viewDidLoad(){
-        super.viewDidLoad()
-        //InputUsuarioContainerView - Adicionando borda redonda
-        inputUsuario.layer.cornerRadius = 10
-        inputUsuario.clipsToBounds = true
-        
-        //RespostaAssistat - Adicionando borda redonda
-        respostaAssistant.layer.cornerRadius = 10
-        respostaAssistant.clipsToBounds = true
-        
-        //buscarResposta - Adicionando borda redonda
-        buscarResposta.layer.cornerRadius = 10
-        buscarResposta.clipsToBounds = true
-    }
+class ViewController: UIViewController, UITextFieldDelegate {
     
     var userQuestions: [String] = []
     
-   
+    private func setupUI() {
+        inputUsuario.layer.cornerRadius = 10
+        inputUsuario.clipsToBounds = true
+        respostaAssistant.layer.cornerRadius = 10
+        respostaAssistant.clipsToBounds = true
+        buscarResposta.layer.cornerRadius = 10
+        buscarResposta.clipsToBounds = true
+        respostaAssistant.isHidden = true
+        inputUsuario.isHidden = true
+    }
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        digiteSuaPergunta.delegate = self
+        
+        setupUI()
+        setupKeyboardNotifications()
+        
+        
+    }
+    
+    private func setupKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
+            return
+        }
+        let adjustedContainerFrame = containerView.frame.offsetBy(dx: 0, dy: -keyboardFrame.height + 0.15 * keyboardFrame.height)
+        containerView.frame = adjustedContainerFrame
+    }
+    
+    @IBOutlet weak var containerView: UIView!
+    
     @IBOutlet weak var previousQuestionsButton: UIButton!
     
     @IBOutlet weak var digiteSuaPergunta: UITextField!
@@ -38,77 +58,74 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var buscarResposta: UIButton!
     
-
     @IBAction func buscarResposta(_ sender: UIButton) {
         //let apiKey = processInfo.environment["apiKey"]
-        let apiKey = ""
-        inputUsuario.text = digiteSuaPergunta.text
-                
         guard let question = digiteSuaPergunta.text, !question.isEmpty else {
             return
+        }
+        inputUsuario.text = question
+        inputUsuario.isHidden = false
+        respostaAssistant.isHidden = false
+        respostaAssistant.text = ""
         
-  }
         userQuestions.append(question)
         digiteSuaPergunta.text = ""
         
-        respostaAssistant.isEditable = false
-        respostaAssistant.isScrollEnabled = false
+        performRequest(for: question)
+    }
+    
+    
+    private func performRequest(for question: String) {
+        guard let url = URL(string: "") else {
+            print("URL inválida")
+            return
+        }
         
-        let url = URL(string: "")!
         var request = URLRequest(url: url)
-             request.httpMethod = "POST"
-             request.setValue("application/json",
-                    forHTTPHeaderField: "Content-Type")
-             request.setValue(apiKey, forHTTPHeaderField: "apikey")
-             
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("ApiKey aqui", forHTTPHeaderField: "apikey")
+        
         let parameters: [String: Any] = [
             "question": question,
             "user_session": [
-            "metadata": [
-            "user_id": "string"
-                     ],
-            "system": [
-                "session_id": "string"
-                     ]
-                 ]
-             ]
-        request.httpBody = try? JSONSerialization.data(withJSONObject: parameters)
-             
+                "metadata": ["user_id": "string"],
+                "system": ["session_id": "string"]
+            ]
+        ]
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters)
+        } catch {
+            print("Erro ao serializar JSON:", error)
+            return
+        }
+        
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error {
-        print("Error:", error.localizedDescription)
+                print("Erro na chamada de rede:", error.localizedDescription)
                 return
-                 }
-                 
+            }
+            
             if let data = data {
                 do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-                    if let answer = json?["answer"] as? String {
-                        print("Resposta:", answer)
-                        DispatchQueue.main.sync {
-                            self.respostaAssistant.text = answer
-                             }
-                         }
-                     } catch {
-                         print("Error parsing JSON:", error.localizedDescription)
-                     }
-                 }
-             }.resume()
-             
-             
-             digiteSuaPergunta.text = ""
-            print (userQuestions)
-        
-        
-         }
-   
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        if let answer = json["answer"] as? String {
+                            DispatchQueue.main.async {
+                                self.respostaAssistant.text = answer
+                            }
+                        }
+                    }
+                } catch {
+                    print("Erro no parsing do JSON:", error.localizedDescription)
+                }
+            }
+        }.resume()
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "PreviousQuestionsSegue", let destination = segue.destination as? PreviousQuestionsController {
             destination.previousQuestions = userQuestions
         }
     }
-    
-
-   }
-    
-
+}
